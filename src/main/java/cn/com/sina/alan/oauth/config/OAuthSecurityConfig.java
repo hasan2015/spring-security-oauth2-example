ec5e51fd9ae7e480867e242c0161bc5d638b25d3
yanhaixun <yanhaixun@360ser.com>
2017-03-16 16:13:19 +0800

    本地调通 1、inset into 2、 localhost:8080/oauth/authorize?client_id=client&response_type=code&redirect_uri=http://www.baidu.com

--- src/main/java/cn/com/sina/alan/oauth/config/OAuthSecurityConfig.java
+++ src/main/java/cn/com/sina/alan/oauth/config/OAuthSecurityConfig.java
@@ -17,61 +17,59 @@ import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import javax.sql.DataSource;
 import java.util.concurrent.TimeUnit;
 
-
 /**
  * Created by wanghongfei(hongfei7@staff.sina.com.cn) on 9/12/16.
  */
 @Configuration
 public class OAuthSecurityConfig extends AuthorizationServerConfigurerAdapter {
-    @Autowired
-    private AuthenticationManager authenticationManager;
+	@Autowired
+	private AuthenticationManager authenticationManager;
 
-    @Autowired
-    private DataSource dataSource;
+	@Autowired
+	private DataSource dataSource;
 
-    @Bean
-    public TokenStore tokenStore() {
-        return new JdbcTokenStore(dataSource);
-    }
+	// 声明TokenStore实现
+	@Bean
+	public TokenStore tokenStore() {
+		return new JdbcTokenStore(dataSource);
+	}
+	// 声明 ClientDetails实现
+	@Bean
+	public ClientDetailsService clientDetails() {
+		return new JdbcClientDetailsService(dataSource);
+	}
+	 // 配置框架应用上述实现
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(authenticationManager);
+		endpoints.tokenStore(tokenStore());
+
+		// 配置TokenServices参数
+		DefaultTokenServices tokenServices = new DefaultTokenServices();
+		tokenServices.setTokenStore(endpoints.getTokenStore());
+		tokenServices.setSupportRefreshToken(false);
+		tokenServices.setClientDetailsService(endpoints.getClientDetailsService());
+		tokenServices.setTokenEnhancer(endpoints.getTokenEnhancer());
+		tokenServices.setAccessTokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(30)); // 30天
+		endpoints.tokenServices(tokenServices);
+
+	}
+
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+		// oauthServer.checkTokenAccess("isAuthenticated()");
+		oauthServer.checkTokenAccess("permitAll()");
+		oauthServer.allowFormAuthenticationForClients();
+	}
 
 
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
-        endpoints.tokenStore(tokenStore());
-
-        // 配置TokenServices参数
-        DefaultTokenServices tokenServices = new DefaultTokenServices();
-        tokenServices.setTokenStore(endpoints.getTokenStore());
-        tokenServices.setSupportRefreshToken(false);
-        tokenServices.setClientDetailsService(endpoints.getClientDetailsService());
-        tokenServices.setTokenEnhancer(endpoints.getTokenEnhancer());
-        tokenServices.setAccessTokenValiditySeconds( (int) TimeUnit.DAYS.toSeconds(30)); // 30天
-        endpoints.tokenServices(tokenServices);
-
-    }
-
-
-    @Override
-    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
-        //oauthServer.checkTokenAccess("isAuthenticated()");
-        oauthServer.checkTokenAccess("permitAll()");
-        oauthServer.allowFormAuthenticationForClients();
-    }
-
-    @Bean
-    public ClientDetailsService clientDetails() {
-        return new JdbcClientDetailsService(dataSource);
-    }
-
-    @Override
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.withClientDetails(clientDetails());
-/*        clients.inMemory()
-                .withClient("client")
-                .secret("secret")
-                .authorizedGrantTypes("authorization_code")
-                .scopes("app");*/
-    }
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.withClientDetails(clientDetails());
+		/*
+		 * clients.inMemory() .withClient("client") .secret("secret")
+		 * .authorizedGrantTypes("authorization_code") .scopes("app");
+		 */
+	}
 
 }
